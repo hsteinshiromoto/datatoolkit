a99f72543a924eecbef1db84777e665524822d23
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-10-06 10:58:23 +1100

    add networkx as depedencies

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     ],
     packages=["datatoolkit"],
     include_package_data=True,
-    install_requires=["pandas", "seaborn", "typeguard", "statsmodels", "bokeh", "scipy", "numpy"],
+    install_requires=["pandas", "seaborn", "typeguard", "statsmodels", "bokeh", "scipy", "numpy", "networkx"],
     entry_points={
         "console_scripts": [
             "datatoolkit=datatoolkit.__main__:main",
