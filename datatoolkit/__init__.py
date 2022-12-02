38a68c7f42dfa5161832fcab8dac6a82aab87ef3
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2022-12-02 16:48:59 +1100

    integrate visualize into __init__.py

--- datatoolkit/__init__.py
+++ datatoolkit/__init__.py
@@ -3,3 +3,4 @@ __version__='0.1.0'
 from datatoolkit.utils import Quantize, QuantizeDatetime, MostFrequent
 from datatoolkit.mock_dataset import DataTypes, MockData
 from datatoolkit.hypothesis import SingleSampleTest, TwoSampleTest
+from datatoolkit.visualize import heatmap_4d, line_bar_plot, dash_line, hist_box, graphplot
