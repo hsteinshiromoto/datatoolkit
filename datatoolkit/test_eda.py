c32190871d16b70ab7b68ed2536abfbcf6cba195
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2023-11-07 14:00:53 +1100

    Add Summarize class to test_eda.py

--- datatoolkit/test_eda.py
+++ datatoolkit/test_eda.py
@@ -4,11 +4,12 @@ from pathlib import Path
 import pandas as pd
 import pytest
 
+
 PROJECT_ROOT = Path(__file__).parents[1].resolve()
 
 sys.path.append(str(PROJECT_ROOT))
 
-from datatoolkit import Group, mock_dataset
+from datatoolkit import Group, mock_dataset, Summarize
 
 
 @pytest.fixture(scope="module")
@@ -33,4 +34,44 @@ def test_Group_make_groups(get_data):
     group = Group(feature="float_0", by=["categorical_0"], data=data)
     group.make_groups()
 
-    assert isinstance(group.grouped, pd.core.groupby.SeriesGroupBy)
+    assert isinstance(group.grouped, pd.core.groupby.SeriesGroupBy)import sys
+
+
+class TestSummarize:
+    def test_get_count(self, get_data):
+        data = get_data
+        summarize = Summarize(feature="float_0", by=["categorical_0"], data=data)
+        summarize.get_count()
+
+        assert isinstance(summarize.summarized_data, pd.DataFrame)
+        assert "count_float_0" in summarize.summarized_data.columns
+        assert "cum_count_float_0" in summarize.summarized_data.columns
+
+    def test_get_proportion(self, get_data):
+        data = get_data
+        summarize = Summarize(feature="float_0", by=["categorical_0"], data=data)
+        summarize.get_proportion()
+
+        assert isinstance(summarize.summarized_data, pd.DataFrame)
+        assert "proportions_float_0" in summarize.summarized_data.columns
+        assert "cum_proportions_float_0" in summarize.summarized_data.columns
+
+    def test_get_entropy(self, get_data):
+        data = get_data
+        summarize = Summarize(feature="float_0", by=["categorical_0"], data=data)
+        summarize.get_entropy()
+
+        assert isinstance(summarize.summarized_data, pd.DataFrame)
+        assert "entropy_float_0" in summarize.summarized_data.columns
+
+    def test_get_summary(self, get_data):
+        data = get_data
+        summarize = Summarize(feature="float_0", by=["categorical_0"], data=data)
+        summarize.get_summary()
+
+        assert isinstance(summarize.summarized_data, pd.DataFrame)
+        assert "count_float_0" in summarize.summarized_data.columns
+        assert "cum_count_float_0" in summarize.summarized_data.columns
+        assert "proportions_float_0" in summarize.summarized_data.columns
+        assert "cum_proportions_float_0" in summarize.summarized_data.columns
+        assert "entropy_float_0" in summarize.summarized_data.columns
