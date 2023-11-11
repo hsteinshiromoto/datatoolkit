Fix binning issue in Numerical class constructor

datatoolkit/eda.py
@@ -248,7 +248,7 @@ class Discretize(Summarize):
             bin_edges = self.get_bin_edges(by, data, bins)
             groupby_args = self.make_groupby_args(by, data, bin_edges)
             super().__init__(feature=feature, by=groupby_args, data=data)
-            
+
         else:
             super().__init__(feature=feature, by=by, data=data)
 
@@ -313,7 +313,7 @@ class Numerical(Discretize):
         feature: str,
         by: Union[str, Iterable[Union[np.number, str, pd.api.types.CategoricalDtype]]],
         data: pd.DataFrame,
-        bins: Union[Sequence, str, int] = "auto",
+        bins: Union[Sequence, str, int] = None,
         summary_dict: dict = {},
     ):
         super().__init__(feature=feature, by=by, data=data)